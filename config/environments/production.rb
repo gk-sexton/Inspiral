b41383d7420ca89eea48449edfeb3d12470e23a8
Greg Sexton <gksexton1@gmail.com>
2016-12-06 17:25:14 -0500

    Prep for heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.serve_static_files = true
+  config.log_level = :debug 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
