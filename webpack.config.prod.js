d8248fd3b3cb1b394ed6658c60c08982c111e7c6
Greg Sexton <gksexton1@gmail.com>
2017-01-12 16:00:29 -0500

    Test new production webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,39 +1,78 @@
-var webpack = require('./webpack.config.js');
-var path = require("path");
+// var webpack = require('./webpack.config.js');
+// var path = require("path");
+//
+// module.exports = {
+//   context: __dirname,
+//   entry: "./frontend/inspiral.jsx",
+//   output: {
+//     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: "bundle.js"
+//   },
+//   plugins:[
+//    new webpack.DefinePlugin({
+//      'process.env':{
+//        'NODE_ENV': JSON.stringify('production')
+//      }
+//    }),
+//    new webpack.optimize.UglifyJsPlugin({
+//      compress:{
+//        warnings: true
+//      }
+//    })
+//  ],
+//   module: {
+//     loaders: [
+//       {
+//         test: [/\.jsx?$/, /\.js?$/],
+//         exclude: /(node_modules|bower_components)/,
+//         loader: 'babel',
+//         query: {
+//           presets: ['es2015', 'react']
+//         }
+//       }
+//     ]
+//   },
+//   devtool: 'source-maps',
+//   resolve: {
+//     extensions: ["", ".js", ".jsx" ]
+//   }
+// };
+
+var webpack = require("webpack");
 
 module.exports = {
   context: __dirname,
   entry: "./frontend/inspiral.jsx",
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    path: "./app/assets/javascripts",
     filename: "bundle.js"
   },
   plugins:[
-   new webpack.DefinePlugin({
-     'process.env':{
-       'NODE_ENV': JSON.stringify('production')
-     }
-   }),
-   new webpack.optimize.UglifyJsPlugin({
-     compress:{
-       warnings: true
-     }
-   })
- ],
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules|bower_components)/,
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['react', 'es2015']
         }
       }
     ]
   },
-  devtool: 'source-maps',
+  devtool: 'source-map',
   resolve: {
-    extensions: ["", ".js", ".jsx" ]
+    extensions: ["", ".js", ".jsx"]
   }
 };
